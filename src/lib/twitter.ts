357f048be0bc329a4cd5beeca37ffd0e8da97361
taroj1205 <taroj1205@gmail.com>
2024-04-26 10:33:16 +1200

    chore: Update npm dependencies and add @types/cookies

--- src/lib/twitter.ts
+++ src/lib/twitter.ts
@@ -1,21 +1,62 @@
 'use server'
 import { Rettiwt, Tweet, User } from "rettiwt-api";
+import Cookies from 'cookies';
 
-const rettiwt = new Rettiwt();
+let cookies: Cookies | null = null;
+
+export const setCookies = (req: any, res: any) => {
+  cookies = new Cookies(req, res);
+};
+
+const getProxyUrl = async () => {
+  let proxyUrl = cookies ? cookies.get('proxyUrl') : null;
+
+  if (!proxyUrl) {
+    try {
+      const response = await fetch('https://api.github.com/repos/monosans/proxy-list/contents/proxies_anonymous/http.txt', {
+        headers: {
+          'Accept': 'application/vnd.github.v3.raw'
+        }
+      });
+      const data = await response.text();
+      const proxyList = data.split('\n');
+      proxyUrl = proxyList[0];
+
+      
+      if (cookies) {
+        cookies.set('proxyUrl', proxyUrl, { maxAge: 30 * 60 * 1000 });
+      }
+    } catch (error) {
+      console.error(`Failed to fetch proxy list: ${(error as Error).message}`);
+      throw new Error(`Failed to fetch proxy list: ${(error as Error).message}`);
+    }
+  }
+
+  return proxyUrl;
+};
 
 export const userProfile = async (id: string) => {
   try {
-    const details = (await rettiwt.user.details(id)) as User;
-    if (details.profileImage) {
-      details.profileImage = details.profileImage.replace('_normal', '');
+    const proxyUrl = await getProxyUrl();
+    const rettiwt = new Rettiwt({ proxyUrl: new URL(`http://${proxyUrl}`), logging: true });
+
+    try {
+      const details = (await rettiwt.user.details(id)) as User;
+      if (details.profileImage) {
+        details.profileImage = details.profileImage.replace('_normal', '');
+      }
+      return details;
+    } catch (error) {
+      throw new Error(`Failed to fetch user profile: ${(error as Error).message}`);
     }
-    return details;
   } catch (error) {
-    throw new Error(`Failed to fetch user profile: ${(error as Error).message}`);
+    throw new Error(`Failed to fetch proxy list: ${(error as Error).message}`);
   }
 };
 
 export const userTweet = async (id: string) => {
+  const proxyUrl = await getProxyUrl()
+  const rettiwt = new Rettiwt({ proxyUrl: new URL(`http://${proxyUrl}`), logging: true });
   try {
     const tweet = (await rettiwt.tweet.details(id)) as Tweet;
     if (tweet.tweetBy.profileImage) {
@@ -28,6 +69,8 @@ export const userTweet = async (id: string) => {
 };
 
 export const userTimeline = async (id: string) => {
+  const proxyUrl = await getProxyUrl()
+  const rettiwt = new Rettiwt({ proxyUrl: new URL(`http://${proxyUrl}`), logging: true });
   try {
     const timeline = await rettiwt.user.timeline(id);
     if (timeline.list[0].tweetBy.profileImage) {
